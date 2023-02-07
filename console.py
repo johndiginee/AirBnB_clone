c8ff7d7f1cdc92faab11f3fa35c7711ef2dd4b02
John Diginee <Johndbizz@gmail.com>
2023-02-07 09:53:53 +0100

    Added source code file for do_count attributes

--- console.py
+++ console.py
@@ -145,3 +145,13 @@ class HBNBCommand(cmd.Cmd):
                 elif len(argl) == 0:
                     objl.append(obj.__str__())
             print(objl)
+
+    def do_count(self, arg):
+        """Usage: count <class> or <class>.count()
+        Retrieve the number of instances of a given class."""
+        argl = parse(arg)
+        count = 0
+        for obj in storage.all().values():
+            if argl[0] == obj.__class__.__name__:
+                count += 1
+        print(count)
